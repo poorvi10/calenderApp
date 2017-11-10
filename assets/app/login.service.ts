3af749c3ac099efec5c335e88c279436f651db70
Poorvi <prvdubey@gmail.com>
2017-11-10 11:03:03 +0530

    http service

--- assets/app/login.service.ts
+++ assets/app/login.service.ts
@@ -1,9 +1,18 @@
+import {Http, Response} from "@angular/http"
 import {Login} from "./login.model";
+import {Injectable} from "@angular/core";
+import {Observable} from "rxjs";
+import "rxjs/Rx";
 
+@Injectable()
 export class LoginService {
     private login: Login[] = [];
 
+    constructor(private http: Http) {}
     addUser(data: Login) {
-        console.log(data);
+        const body = JSON.stringify();
+        this.http.post("http://localhost:3000/", body)
+            .map((response: Response) => response.json())
+            .catch((error:Response) => Observable.throw(error.json()))
     }
 }
--- routes/app.js
+++ routes/app.js
@@ -1,8 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var User = require("../models/signIn");
 
 router.get('/', function (req, res, next) {
     res.render('index');
+});
+
+router.post('/', function (req, res, next) {
+    var user = new User({
+        token: req.body.token,
+        image: req.body.image,
+        name: req.body.name,
+        email: req.body.email,
+    });
+    user.save(function(err, result) {
+        if(err) {
+            return res.status(500).json({
+                title: "An error occured",
+                error: err
+            });
+        }
+        res.status(201).json({
+            message: "User credentials saved",
+            obj: result
+        });
+    });
 });
 
 module.exports = router;
